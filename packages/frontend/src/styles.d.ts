7bf279eb958795ea05034d64c9efa093824cac04
SoftSystemsStudio <softsystemstudioco@gmail.com>
2025-12-02 03:06:14 +0000

    chore(frontend): add CSS module types and make :root global in Home.module.css

---
+++ packages/frontend/src/styles.d.ts
@@ -0,0 +1,10 @@
+// packages/frontend/src/styles.d.ts
+declare module '*.module.css' {
+  const classes: { [key: string]: string };
+  export default classes;
+}
+
+declare module '*.css' {
+  const classes: { [key: string]: string };
+  export default classes;
+}
